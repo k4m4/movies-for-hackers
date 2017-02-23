6b27448ee1d62169ce04acbde00232763feabb5e
MichaelCook <michael@evoluted.net>
2017-02-23 14:52:32 +0000

    Housekeeping

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -38,7 +38,7 @@ objectifyMarkdownNotWomen.table = function(header, body) {
 	movies = [{}];
 };
 
-client.open('GET', window.location.href + '/README.md');
+client.open('GET', window.location.href + 'README.md');
 client.onreadystatechange = function(e) {
 	// Whipe movies and collections as this'll run a bunch of times
 	moviesCollection = [];
@@ -56,8 +56,3 @@ client.onreadystatechange = function(e) {
 	});
 };
 client.send();
-
-function baseUrl() {
-	var href = window.location.href.split('/');
-	return href[0]+'//'+href[2]+'/';
-}

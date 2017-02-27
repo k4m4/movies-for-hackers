66420f73063db5bd490513ad4f9bddc2a544fd4c
MichaelCook <michael@evoluted.net>
2017-02-27 13:47:53 +0000

    Script now has everything the body needs

--- update-movies.bat
+++ update-movies.bat
@@ -3,4 +3,6 @@ echo.
 echo Pull from master - as it has the remote set by git remote add upstream https://github.com/k4m4/movies-for-hackers.git
 echo.
 git pull upstream master
+git pull
+git push
 pause
